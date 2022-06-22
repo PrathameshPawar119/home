c8e8ef115730f48b4824a0e99fba98a117f0a474
Prathamesh Pawar <cm.b26.prathameshpawar@gmail.com>
2022-06-22 13:41:45 +0530

    Updates

--- precache-manifest.cd8f409c24dd8f11f338d1def18ce7b9.js
+++ precache-manifest.cd8f409c24dd8f11f338d1def18ce7b9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6a2efa079b76cca0c374",
+    "revision": "686162bf12f92a67ad45",
     "url": "/textutils/static/css/main.2883125d.chunk.css"
   },
   {
-    "revision": "6a2efa079b76cca0c374",
-    "url": "/textutils/static/js/main.c52e1629.chunk.js"
+    "revision": "686162bf12f92a67ad45",
+    "url": "/textutils/static/js/main.9c378749.chunk.js"
   },
   {
     "revision": "201a5182aef102b75c86",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/textutils/static/js/3.5526bd81.chunk.js"
   },
   {
-    "revision": "26e40e4e827bbec06016b17ec0539797",
+    "revision": "88846fcf075b8bbc6750f30732df16ca",
     "url": "/textutils/index.html"
   }
 ];
