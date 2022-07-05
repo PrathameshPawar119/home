52ff789e074e3cdc7086fd76d1731aef5fa6f383
Prathamesh Pawar <104665278+PrathameshPawar119@users.noreply.github.com>
2022-07-05 20:09:04 +0530

    Add files via upload

---
+++ precache-manifest.e03a80d4bb9c01eb93c5869daf9b4e12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd15e6b6b4198015833d",
+    "url": "/textutils/static/css/main.2883125d.chunk.css"
+  },
+  {
+    "revision": "fd15e6b6b4198015833d",
+    "url": "/textutils/static/js/main.12275f21.chunk.js"
+  },
+  {
+    "revision": "201a5182aef102b75c86",
+    "url": "/textutils/static/js/runtime~main.9ca79808.js"
+  },
+  {
+    "revision": "37afe2c310608fa6c4f5",
+    "url": "/textutils/static/js/2.b18ea381.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/textutils/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "541c75e43f55765125f8a9444dd518bb",
+    "url": "/textutils/index.html"
+  }
+];
